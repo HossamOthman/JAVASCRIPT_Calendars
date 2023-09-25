ad66e056c354f7b1a22a54530c80c3a440110364
Hossam Othman <rowheadrex@gmail.com>
2023-09-25 23:22:29 +0200

    Clock is Done

--- 2nd_Date/app.js
+++ 2nd_Date/app.js
@@ -7,7 +7,27 @@ const today = new Date();
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const AllMonths = ['January', 'Fabruary', 'March', 'April', 'May', 'June', 'July', 'August', 'Septemper', 'October', 'November', 'December']
 
-date.innerHTML = today.getDate() < 10 ? '0' : '' + today.getDate();
+date.innerHTML = today.getDate() < 10 ? '0' +  today.getDate() : today.getDate();
 day.innerHTML = weekDays[today.getDay()];
 month.innerHTML = AllMonths[today.getMonth()];
 year.innerHTML = today.getFullYear();
+
+
+///////////////////////////////////////
+
+
+    function showTime(){
+        'use strict';
+        var now = new Date(),
+            hours = now.getHours() < 10 ? '0' + now.getHours() : now.getHours(),
+            minutes = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes(),
+            seconds = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds();
+
+            document.getElementById('clock').textContent = hours + ': ' + minutes + ': ' + seconds;
+    };
+
+window.onload = function () {
+    'use strict';
+
+    setInterval(showTime, 1000)
+}
