e1e9647aff2c28c834835aff9a355b4b0e12a141
Hossam Othman <rowheadrex@gmail.com>
2023-09-27 18:27:08 +0200

    prev & next days & current

--- 4th_Calendar/app.js
+++ 4th_Calendar/app.js
@@ -4,6 +4,12 @@ date.setDate(1);
 
 const monthDays = document.querySelector('.days');
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const firstDayIndex = date.getDay();
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+const nextDays = lastDayIndex +1;
+
 const months = [
     "January",
     "February",
@@ -24,8 +30,19 @@ document.querySelector('.date p').innerHTML = date.toDateString();;
 
 let days = '';
 
-for (let i = 1; i <= lastDay; i++ ){
-    days += `<div>${i}</div>`;
-    monthDays.innerHTML = days;
-
+for(let x = firstDayIndex; x > 0; x--){
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
 }
+
+for (let i = 1; i <= lastDay; i++ ){
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+        days += `<div class="today">${i}</div>`;
+    } else{
+        days += `<div>${i}</div>`;
+    }
+}
+
+for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
